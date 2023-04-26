c8e805a328fee6f3ca4ca7fa291e84a496bb472a
unknown <jyotiranjans6262@gmail.com>
2023-04-26 20:08:04 +0530

    initial checkin

---
+++ Firstclass/src/com/java/ex/Firstexample.java
@@ -0,0 +1,9 @@
+package com.java.ex;
+
+public class Firstexample {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
---
+++ Firstclass/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author JYOTIRANJAN
+ *
+ */
+module Firstclass {
+}
